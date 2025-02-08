3c4ae760e506061bfe957ac382e9384d78d18562
vlad <mr.xeris@mail.ru>
2025-02-09 00:26:33 +0200

    age proj

---
+++ Lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод возрастов Тани и Мити
+        Console.Write("Введите возраст Тани: ");
+        int ageTanya = int.Parse(Console.ReadLine());
+        
+        Console.Write("Введите возраст Мити: ");
+        int ageMitya = int.Parse(Console.ReadLine());
+        
+        // Вычисление среднего возраста
+        double averageAge = (ageTanya + ageMitya) / 2.0;
+        
+        // Вычисление разницы возрастов с средним значением
+        double diffTanya = Math.Abs(ageTanya - averageAge);
+        double diffMitya = Math.Abs(ageMitya - averageAge);
+        
+        // Вывод результатов
+        Console.WriteLine($"Средний возраст: {averageAge:F2}");
+        Console.WriteLine($"Разница возраста Тани от среднего: {diffTanya:F2}");
+        Console.WriteLine($"Разница возраста Мити от среднего: {diffMitya:F2}");
+    }
+}
